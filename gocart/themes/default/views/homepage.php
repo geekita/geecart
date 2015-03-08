60b533c9c58600763f484ed1e5904c511f6e06a2
geekita <d.migliarino@geekita.com>
2015-03-08 16:40:54 +0100

    Cloned from this last commit https://github.com/gocart/GoCart/commit/324eccb2c4405bc81a5eece96dab06e121b15088

---
+++ gocart/themes/default/views/homepage.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="span12">
+        <?php $this->banners->show_collection(1, 5);?>
+    </div>
+</div>
+
+<?php $this->banners->show_collection(2, 3, '3_box_row');?>
